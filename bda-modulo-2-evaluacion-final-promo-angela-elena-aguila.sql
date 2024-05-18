75b0be22af102c6fdfbde168e2d46a6428e283e0
Elena <eaguilagarcia@gmail.com>
2024-05-18 09:57:27 +0200

    Add statements from 1 to 8 related to tables actor and film from sakila database

--- bda-modulo-2-evaluacion-final-promo-angela-elena-aguila.sql
+++ bda-modulo-2-evaluacion-final-promo-angela-elena-aguila.sql
@@ -1,36 +1,89 @@
 -- Evaluación Final Módulo 2
 
+-- Para este ejerccio utilizaremos la BBDD Sakila
+USE sakila;
+
 -- 1.
 /* Selecciona todos los nombres de las películas
 sin que aparezcan duplicados. */
 
+SELECT DISTINCT title
+FROM sakila.film
+ORDER BY title;
+-- 1000 rows returned
+
 -- 2.
 /* Muestra los nombres de todas las películas
 que tengan una clasificación de "PG-13". */
+
+SELECT title
+FROM sakila.film
+WHERE rating = 'PG-13'
+ORDER BY title;
+-- 223 rows returned
 
 -- 3.
 /* Encuentra el título y la descripción de todas
 las películas que contengan la palabra "amazing"
 en su descripción. */
 
+SELECT title, description
+FROM sakila.film
+WHERE description LIKE '%amazing%'
+ORDER BY title;
+-- 48 rows returned
+
 -- 4.
 /* Encuentra el título de todas las películas que
 tengan una duración mayor a 120 minutos. */
 
+SELECT title
+FROM sakila.film
+WHERE length > 120
+ORDER BY title;
+-- 457 rows returned
+
 -- 5.
 /* Recupera los nombres de todos los actores. */
+
+SELECT DISTINCT
+first_name AS nombre,
+last_name AS apellido
+FROM sakila.actor
+ORDER BY apellido;
+-- 199 rows returned
 
 -- 6.
 /* Encuentra el nombre y apellido de los actores
 que tengan "Gibson" en su apellido. */
 
+SELECT
+first_name AS nombre,
+last_name AS apellido
+FROM sakila.actor
+WHERE last_name REGEXP 'Gibson';
+-- 1 row returned
+
 -- 7.
 /* Encuentra los nombres de los actores que
 tengan un actor_id entre 10 y 20. */
 
+SELECT
+first_name AS nombre,
+last_name AS apellido
+FROM sakila.actor
+WHERE actor_id BETWEEN 10 AND 20;
+-- 11 rows returned
+
 -- 8.
 /* Encuentra el título de las películas en la tabla film
 que no sean ni "R" ni "PG-13" en cuanto a su clasificación. */
+
+SELECT title
+FROM sakila.film
+WHERE rating NOT IN ('R','PG-13')
+ORDER BY title;
+-- 582 rows returned
 
 -- 9.
 /* Encuentra la cantidad total de películas en cada clasificación
